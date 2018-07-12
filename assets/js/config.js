8f3a8ccce53ccc2305162866cc6ba1a72d6e799b
Torning, Fraser <Fraser_Torning@comcast.com>
2018-07-12 16:35:14 -0400

    final sub

---
+++ assets/js/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCFOxor5xaOkhiGivc9wXUTp7s0C6W7mxM",
+    authDomain: "giphy-project-800e4.firebaseapp.com",
+    databaseURL: "https://giphy-project-800e4.firebaseio.com",
+    projectId: "giphy-project-800e4",
+    storageBucket: "giphy-project-800e4.appspot.com",
+    messagingSenderId: "344250435560"
+};
+
+var giphyApiKey = 'T8o85vSTbfj1h4aJtXz15G35cinUT2JZ'
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -10,30 +10,84 @@ try {
     console.log('Failed to initialize database');
 }
 
-// Delete this out - just for reference
-var title = $('#title');
-var wink = $('#wink');
-title.css('text-align', 'center');
-wink.css('height', '30px');
-
 // Go nuts
+var resultsDiv = $('#search-results');
+var recentDiv = $('#recent-searches');
+
 class GiphySearch {
     constructor(searchString) {
-      this.searchString = "&q=" + searchString;
-      
+      this.searchString = searchString;
     }
   
     commit() {
-        db.push(this);
+        db.ref("searches").push(this);
     }
 
     getGifs() {
         $.ajax({
-            url: "http://api.giphy.com/v1/gifs/search?api_key=" + giphyApiKey + "&limit=20&q=" + this.searchString,
+            url: "https://api.giphy.com/v1/gifs/search?api_key=" + giphyApiKey + "&limit=10&q=" + this.searchString,
             method: 'GET'
         }).then(function(response) {
-            console.log(response);
+            var data = response.data;
+            console.log(data[0]);
+            for (var i = 0; i < data.length; i++) {
+                var gifElem = $('<div>');
+                gifElem.addClass(['col-sm-6', 'col-md-4', 'col-lg-3']);
+                var gifRating = $('<p>');
+                gifRating.text('Rating: ' + data[i].rating);
+                var gifImg = $('<img>');
+                gifImg.attr("src", data[i].images.fixed_width_still.url);
+                gifImg.attr("data-static", data[i].images.fixed_width_still.url);
+                gifImg.attr("data-dynamic", data[i].images.fixed_width.url);
+                gifElem.append([gifRating, gifImg]);
+                resultsDiv.append(gifElem);
+
+            }
         })
     }
-  
-  }
+}
+
+$('body').on('click', 'img', function() {
+    var clicked = $($(this)[0]);
+    if (clicked.attr('src') === clicked.attr('data-static')) {
+        clicked.attr('src', clicked.attr('data-dynamic'));
+    } else {
+        clicked.attr('src', clicked.attr('data-static'));
+    }    
+});
+
+$("#run-search").on("click", function(event){
+
+    if (searchParam === ""){
+        alert("search can't be null");
+    
+    } else {
+        event.preventDefault();
+        var searchParam = $("#search-term").val().trim();
+        var newSearch = new GiphySearch(searchParam);
+        resultsDiv.empty();
+        newSearch.commit();
+        newSearch.getGifs()
+    }
+});
+
+db.ref('searches').on("child_added", function(snapshot) {
+    var newItem = $('<button>');
+    newItem.text(snapshot.val().searchString)
+    newItem.addClass(["btn", "btn-success"]);
+    newItem.css('margin-left', '5px');
+    newItem.css('margin-right', '5px');
+    recentDiv.prepend(newItem);
+    
+       
+});
+
+recentDiv.on('click', '.btn', function() {
+    var clicked = $($(this)[0]);
+    var searchParam = clicked.text();
+    var newSearch = new GiphySearch(searchParam);
+        resultsDiv.empty();
+        newSearch.getGifs()
+    
+});
+
